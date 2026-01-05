bd5412596c67e15fb43b61ad1dce4fde95d4d3ba
20all <bishallimbu00010@gmail.com>
2026-01-06 04:00:43 +0545

    added cloudinary configuration & service

---
+++ ByaparKarobar/ByaparKarobar/src/main/java/com/_all/ByaparKarobar/service/cloudinary/CloudinaryService.java
@@ -0,0 +1,34 @@
+package com._all.ByaparKarobar.service.cloudinary;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadImage(MultipartFile file) {
+        try {
+            Map<String, Object> uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.asMap("resource_type", "auto")
+            );
+
+
+            return uploadResult.get("secure_url").toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Image upload failed", e);
+        }
+    }
+}
