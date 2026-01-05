dd6bc47779d1170e97ff1a6a353293653bf57b1a
20all <bishallimbu00010@gmail.com>
2026-01-05 22:52:03 +0545

    added product service code

--- ByaparKarobar/ByaparKarobar/src/main/java/com/_all/ByaparKarobar/service/impl/ProductServiceImpl.java
+++ ByaparKarobar/ByaparKarobar/src/main/java/com/_all/ByaparKarobar/service/impl/ProductServiceImpl.java
@@ -1,4 +1,58 @@
 package com._all.ByaparKarobar.service.impl;
 
-public class ProductServiceImpl {
+import com._all.ByaparKarobar.dto.Response;
+import com._all.ByaparKarobar.mapper.EntityDtoMapper;
+import com._all.ByaparKarobar.repository.CategoryRepo;
+import com._all.ByaparKarobar.repository.ProductRepo;
+import com._all.ByaparKarobar.service.interf.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepo productRepo;
+    private final CategoryRepo categoryRepo;
+    private final EntityDtoMapper entityDtoMapper;
+
+    @Override
+    public Response createProduct(Long categoryId, MultipartFile image, String name, String description, BigDecimal price) {
+        return null;
+    }
+
+    @Override
+    public Response updateProduct(Long productId, Long categoryId, MultipartFile image, String name, String description, BigDecimal price) {
+        return null;
+    }
+
+    @Override
+    public Response deleteProduct(Long productId) {
+        return null;
+    }
+
+    @Override
+    public Response getProductById(Long productId) {
+        return null;
+    }
+
+    @Override
+    public Response getAllProduct() {
+        return null;
+    }
+
+    @Override
+    public Response getProductByCategory(Long categoryId) {
+        return null;
+    }
+
+    @Override
+    public Response searchProduct(String searchValue) {
+        return null;
+    }
 }
--- ByaparKarobar/ByaparKarobar/src/main/java/com/_all/ByaparKarobar/service/interf/ProductService.java
+++ ByaparKarobar/ByaparKarobar/src/main/java/com/_all/ByaparKarobar/service/interf/ProductService.java
@@ -1,4 +1,25 @@
 package com._all.ByaparKarobar.service.interf;
 
+import com._all.ByaparKarobar.dto.Response;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
 public interface ProductService {
+
+    Response createProduct(Long categoryId, MultipartFile image, String name, String description, BigDecimal price);
+
+    Response updateProduct(Long productId, Long categoryId, MultipartFile image, String name, String description, BigDecimal price);
+
+    Response deleteProduct(Long productId);
+
+    Response getProductById(Long productId);
+
+    Response getAllProduct();
+
+    Response getProductByCategory(Long categoryId);
+
+    Response searchProduct(String searchValue);
+
+
 }
