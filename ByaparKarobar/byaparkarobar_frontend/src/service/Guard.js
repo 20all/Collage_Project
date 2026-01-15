f5b933f2c138d79f75d66821ce9aec9b297c9deb
20all <bishallimbu00010@gmail.com>
2026-01-15 10:28:22 +0545

    added guard service in frontend of byaparKarobar

---
+++ ByaparKarobar/byaparkarobar_frontend/src/service/Guard.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import ApiService from "./ApiService";
+
+
+export const protectedRoute = ({element: Component}) => {
+    const location = useLocation()
+    return ApiService.isAuthenticated() ? (
+        Component
+    ) : (
+        <Navigate to="/login" replace state={{from: location}} />
+    )
+}
+
+export const MerchantRoute = ({element: Component}) => {
+    const location = useLocation()
+    return ApiService.isMerchant() ? (
+        Component
+    ) : (
+        <Navigate to="/login" replace state={{from: location}} />
+    )
+}
